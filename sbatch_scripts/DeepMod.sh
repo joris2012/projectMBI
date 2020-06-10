c36cde68c9c77dcec515ab8c6b826c656c412516
DaanvandenBiggelaar <66366246+DaanvandenBiggelaar@users.noreply.github.com>
2020-06-10 20:41:58 +0200

    Update DeepMod.sh

--- sbatch_scripts/DeepMod.sh
+++ sbatch_scripts/DeepMod.sh
@@ -13,7 +13,7 @@ cd $TMPDIR
 ls -la
 
 #Call methylation with DeepMod
-python DeepMod/bin/DeepMod.py detect --wrkBase test1.0/ecoli_2kb_region/fast5_files/odw_genlab4209_20161213_FN_MN16303_sequencing_run_sample_id_32395_ch --Ref test1.0/ref.fa --outFolder out_folder/test1.0 --Base C --modfile DeepMod/train_mod/rnn_f7_wd21_chr1to10_4/mod_train_f7_wd21_chr1to10 --FileID atgm2002_test1 --threads 16
+python DeepMod/bin/DeepMod.py detect --wrkBase test1.0/ecoli_2kb_region/fast5_files/odw_genlab4209_20161213_FN_MN16303_sequencing_run_sample_id_32395_ch --Ref test1.0/ref.fa --FileID atgm2002_test1 --modfile DeepMod/train_mod/rnn_sinmodC_P100wd21_f7ne1u0_4/mod_train_sinmodC_P100wd21_f3ne1u0 --threads 15 --outFolder out_folder/test1.0
 
 #See if succes
 ls -la
