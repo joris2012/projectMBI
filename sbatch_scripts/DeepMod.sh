a3b66c5e5161dd75c6f2915aa5a4f657964fd581
DaanvandenBiggelaar <66366246+DaanvandenBiggelaar@users.noreply.github.com>
2020-06-10 16:54:17 +0200

    Update DeepMod.sh

--- sbatch_scripts/DeepMod.sh
+++ sbatch_scripts/DeepMod.sh
@@ -12,7 +12,7 @@ cp -r ./* $TMPDIR
 cd $TMPDIR
 ls -la
 
-#Call methylation with deepsignal
+#Call methylation with DeepMod
 python DeepMod/bin/DeepMod.py detect --wrkBase test1.0/ecoli_2kb_region/fast5_files/odw_genlab4209_20161213_FN_MN16303_sequencing_run_sample_id_32395_ch --Ref test1.0/ref.fa --outFolder out_folder/test1.0 --Base C --modfile DeepMod/train_mod/rnn_f7_wd21_chr1to10_4/mod_train_f7_wd21_chr1to10 --FileID atgm2002_test1 --threads 16
 
 #See if succes
